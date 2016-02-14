85252566fd3e960ca757a8afc034c082d8cb3738
Todd Elvers <toddelvers@carfax.com>
2016-02-14 14:10:37 -0600

    Added URLEncoding for zoneName's and removed some duplicate code

--- src/main/groovy/te/light_jockey/rest_services/SonosService.groovy
+++ src/main/groovy/te/light_jockey/rest_services/SonosService.groovy
@@ -1,10 +1,8 @@
 package te.light_jockey.rest_services
 
-import groovy.json.JsonSlurper
 import te.light_jockey.domain.sonos.SonosZoneStatus
 import wslite.rest.RESTClient
 import wslite.rest.Response
-
 
 class SonosService {
     final RESTClient sonosApiEndpoint
@@ -14,8 +12,9 @@ class SonosService {
     }
 
     SonosZoneStatus getZoneStatus(String zoneName) {
-        Response sonosZoneResponse = sonosApiEndpoint.get(path: "/$zoneName/state".replaceAll("\\s", "%20"))
-        def zone = new JsonSlurper().parse(sonosZoneResponse.data)
-        return new SonosZoneStatus(zone)
+        String urlEncodedZoneName = URLEncoder.encode(zoneName, "UTF-8")
+        Response sonosZoneResponse = sonosApiEndpoint.get(path: "/$urlEncodedZoneName/state")
+        return new SonosZoneStatus(sonosZoneResponse.json)
     }
+
 }
