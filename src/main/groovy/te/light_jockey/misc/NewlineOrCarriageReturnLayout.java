958191e88a578b95d9cd445f654bf89273b83500
Todd Elvers <toddelvers@carfax.com>
2016-02-15 18:34:57 -0600

    Fixed an obvious flaw in the logging logic.  This will require testing at Hunter's house.

--- src/main/groovy/te/light_jockey/misc/NewlineOrCarriageReturnLayout.java
+++ src/main/groovy/te/light_jockey/misc/NewlineOrCarriageReturnLayout.java
@@ -12,31 +12,49 @@ public class NewlineOrCarriageReturnLayout extends LayoutBase<ILoggingEvent> {
     private static final String CARRIAGE_RETURN = "\r";
     private static final String ELLIPSIS = "...";
 
-    private boolean lastLogMessageContainedCarriageReturn = false;
+    private boolean prevLogMessageStartedWithCarriageReturn = false;
+
     /**
      * <p>This layout either simply returns the log message untouched, or it returns the log message
      * with a newline character appended to it.
      *
-     * <p><b>1st Case:</b> If a log message <b>does not</b> start with a carriage return <b>or</b> end
-     * with an ellipsis, then the log message is returned with a newline character appended to it.
+     * <p><b>1st Case:</b> If a log message <b>does not</b> start with a carriage return <b>or</b>
+     * end with an ellipsis, then the log message is returned with a newline character appended to
+     * it.
      *
      * <p><b>2nd Case:</b> If a log message starts with a carriage return or ends with an ellipsis,
-     * then the log message is returned from this method with a right-padding of 50 spaces to replace
-     * any remnants of previous log messages that may have been longer in length than the current
-     * log message.
+     * then the log message is returned from this method with a right-padding of 50 spaces to
+     * replace any remnants of previous log messages that may have been longer in length than the
+     * current log message.
      *
      * @param event the logging event
      * @return the message that will be passed to the logger
      */
     public String doLayout(ILoggingEvent event) {
-        if(event.getFormattedMessage().startsWith(CARRIAGE_RETURN)) {
-            return StringUtils.rightPad(event.getFormattedMessage(), CARRIAGE_RETURN_MESSAGE_PADDING_LENGTH, " ");
-        } else if(event.getFormattedMessage().endsWith(ELLIPSIS)) {
-            return event.getFormattedMessage();
+        String logMessage = event.getFormattedMessage();
+
+        if (logMessage.startsWith(CARRIAGE_RETURN)) {
+            prevLogMessageStartedWithCarriageReturn = true;
+            return StringUtils.rightPad(logMessage, CARRIAGE_RETURN_MESSAGE_PADDING_LENGTH, " ");
         } else {
-            String prefix = lastLogMessageContainedCarriageReturn ? " " : "";
-            lastLogMessageContainedCarriageReturn = false;
-            return prefix + event.getFormattedMessage() + CoreConstants.LINE_SEPARATOR;
+            String prefix = determineLogMessagePrefix();
+            prevLogMessageStartedWithCarriageReturn = false;
+            if (logMessage.endsWith(ELLIPSIS)) {
+                return prefix + logMessage;
+            } else {
+                return prefix + logMessage + CoreConstants.LINE_SEPARATOR;
+            }
         }
+    }
+
+    /**
+     * This exists because if the previous log message started with a /r then the first
+     * character of the next log message will be 'consumed' by the /r. We get around this by
+     * simply providing a single space if the previous log message started with a /r.
+     *
+     * @return either a single space or the empty string
+     */
+    private String determineLogMessagePrefix() {
+        return prevLogMessageStartedWithCarriageReturn ? " " : "";
     }
 }
